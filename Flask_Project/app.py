323e9d0ac61ad772f455d4a77d707ad0f1b29593
Suhaan2485 <suhaanbalpande@icloud.com>
2026-06-18 14:45:51 +0530

    test jininja

--- Flask_Project/app.py
+++ Flask_Project/app.py
@@ -4,5 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+@app.route("/")
+def index():
     user_name = "Suhaan"
     return render_template("index.html", user_name=user_name)
