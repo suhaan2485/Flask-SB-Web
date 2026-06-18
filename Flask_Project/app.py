1b1a4eaf25767cbc7c875f7026f61da5aac50655
Suhaan2485 <suhaanbalpande@icloud.com>
2026-06-18 15:03:54 +0530

    test4

--- Flask_Project/app.py
+++ Flask_Project/app.py
@@ -12,7 +12,7 @@ def index():
 @app.route("/testing")
 def testing():
     user_name = "Sharvani"
-    return render_template("index.html", user_name=user_name)
+    return render_template("testing.html", user_name=user_name)
 
 if __name__ == "__main__":
     app.run(debug=True, port = 5003)
