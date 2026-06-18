f6a5a5b1e6df7b2a769e8893fec30f59329e267e
Suhaan2485 <suhaanbalpande@icloud.com>
2026-06-18 22:28:28 +0530

    Doing My Own Stuff!

--- Flask_Project/app.py
+++ Flask_Project/app.py
@@ -5,14 +5,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    user_name = "Suhaan"
-    return render_template("index.html", user_name=user_name)
+    project_name= "Suhaan's Web Portal"
+    return render_template("index.html", project_name=project_name)
 
    
 @app.route("/testing")
 def testing():
-    user_name = "Sharvani"
-    return render_template("testing.html", user_name=user_name)
+    test_display = "Advanced Web Features in progres....."
+    return render_template("testing.html", test_display=test_display)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port = 5003)
