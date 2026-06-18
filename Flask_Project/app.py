46e22b0596d1434273101bbd00abf590cfe3324c
Suhaan2485 <suhaanbalpande@icloud.com>
2026-06-18 14:49:36 +0530

    test1

--- Flask_Project/app.py
+++ Flask_Project/app.py
@@ -2,15 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
 
 @app.route("/")
 def index():
     user_name = "Suhaan"
     return render_template("index.html", user_name=user_name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+   
